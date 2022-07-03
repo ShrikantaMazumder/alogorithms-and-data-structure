a7d5bb706bd1487383c81fed6aaa6cd485f29e75
Shrikanta Mazumder <shrikanta@genuino.world>
2022-07-04 01:58:06 +0600

    bubble sort

--- 2.sorting_algorithm/bubble_sort.py
+++ 2.sorting_algorithm/bubble_sort.py
@@ -8,7 +8,7 @@
 
 # Time Complexity
 # this one similar to selection sort
-# O(n square)
+# O(n^2)
 
 # Space Complexity
 # O(1)
@@ -16,8 +16,16 @@
 # The best case of time complexity for this algo is O(n). If the list is sorted
 
 def bubble_sort(array):
+    # let start the loop from the first element
+    # and run it till the last element
     for i in range(len(array)):
+        # Consider it as a right pointer
+        # We won't consider the sorted elements
+        # that's why stop the inner loop before sorted elements
         for j in range(0, len(array) - i - 1):
+            # check if current element is greater than next one.
+            # then swap it with next one
+            # The bigger ones will go to the last place respectively
             if array[j] > array[j+1]:
                 (array[j], array[j+1]) = (array[j+1], array[j])
 
---
+++ 2.sorting_algorithm/go/bubble_sort.go
@@ -0,0 +1,45 @@
+// Bubble sort
+
+// this will compare between two element
+// between n and n+1
+// if n is bigger than n+1 then they will be swapped (small to large sort)
+// second loop will run (n - i - 1).
+// because we don't need to check the last one that already sorted
+
+// Time Complexity
+// this one similar to selection sort
+// O(n^2)
+
+// Space Complexity
+// O(1)
+
+// The best case of time complexity for this algo is O(n). If the list is sorted
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{5,9,23,53,3,39,100,18, -1, -10};
+	bubble_sort(numbers);
+	fmt.Println(numbers);
+}
+
+func bubble_sort(arr []int) {
+	// let start the loop from the first element
+    // and run it till the last element
+  for i := 0; i < len(arr); i++ {
+	 // Consider it as a right pointer
+        // We won't consider the sorted elements
+        // that's why stop the inner loop before sorted elements
+    for j := 0; j < len(arr) - i - 1; j++ {
+		 // check if current element is greater than next one.
+            // then swap it with next one
+            // The bigger ones will go to the last place respectively
+      if arr[j] > arr[j+1] {
+        temp := arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
